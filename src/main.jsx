49080c459c8c134066ccb1ecd1e0dbb75d3de756
Chiriac <andrei-chiriac@outlook.com>
2023-11-25 11:47:11 +0100

    Version Bump

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "./assets/styles/styles.css";
 import Root from './Root.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
