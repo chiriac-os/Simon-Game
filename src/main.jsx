c60fe694c35ba0b95b44faddc87c12ce85deee06
Chiriac <andrei-chiriac@outlook.com>
2023-11-25 14:55:58 +0100

    Fix: typo CSS file

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from './Root.jsx';
-import "./assets/styles/main.scss";
+import "./assets/styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
